fcf86f941ecf64cce375c3ccec2572c8ff20b1d0
davento <daniela.vento@utec.edu.pe>
2022-10-20 21:47:23 -0500

    updated path names for train and test files

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ from src import model, lstm_ns_random_model, lstm_ns_naive_group, lstm_ns_cluste
 from os import path
 
 if __name__ == "__main__":
-    train_file_path = "datasets/bridge_to_algebra_2008_2009/sample_bridge_to_algebra_2008_2009_train.txt"
-    test_file_path = "datasets/bridge_to_algebra_2008_2009/sample_bridge_to_algebra_2008_2009_test.txt"
+    train_file_path = "datasets/bridge_to_algebra_2008_2009/bridge_to_algebra_2008_2009_train.txt"
+    test_file_path = "datasets/bridge_to_algebra_2008_2009/bridge_to_algebra_2008_2009_test.txt"
 
     data_processor = data_preprocessor.DataPreprocessor(input_file_path=train_file_path)
     data_processor.analyze_dataset()
